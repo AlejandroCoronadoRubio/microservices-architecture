55b4492e691e981cf90186f734e0242840ca92d0
AlejandroCoronadoRubio <alejandrocoronadorubio@gmail.com>
2025-07-21 18:12:53 -0700

    Initial commit

---
+++ src/main/java/com/alejandro/microservices_architecture/MicroservicesArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.alejandro.microservices_architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesArchitectureApplication.class, args);
+	}
+
+}
