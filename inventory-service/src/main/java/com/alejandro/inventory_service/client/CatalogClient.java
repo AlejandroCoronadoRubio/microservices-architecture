1980c1442a5c0ee328414bc2b109f5cc423c7ab3
AlejandroCoronadoRubio <alejandrocoronadorubio@gmail.com>
2025-07-23 01:09:16 -0700

    Implemented circuit breaker with resilience4j

--- inventory-service/src/main/java/com/alejandro/inventory_service/client/CatalogClient.java
+++ inventory-service/src/main/java/com/alejandro/inventory_service/client/CatalogClient.java
@@ -1,6 +1,7 @@
 package com.alejandro.inventory_service.client;
 
 import com.alejandro.inventory_service.domain.Product;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +13,18 @@ import java.util.List;
 public interface CatalogClient {
 
     @GetMapping("/catalog/sku/{sku}")
+    @CircuitBreaker(name = "getProductsBySku", fallbackMethod = "fallbackCatalogBySku")
     ResponseEntity<List<Product>> getProductsBySku(@PathVariable(name = "sku") String sku);
 
     @GetMapping("/catalog/uniqId/{uniqId}")
+    @CircuitBreaker(name = "getProductsByUniqId", fallbackMethod = "fallbackCatalogByUniqId")
     ResponseEntity<List<Product>> getProductsByUniqId(@PathVariable(name = "uniqId") String uniqId);
+
+    default List<Product> fallbackCatalogBySku(String sku, Throwable ex) {
+        return List.of(new Product());
+    }
+
+    default List<Product> fallbackCatalogByUniqId(String uniqId, Throwable ex) {
+        return List.of(new Product());
+    }
 }
--- product-service/src/main/java/com/alejandro/product_service/client/InventoryClient.java
+++ product-service/src/main/java/com/alejandro/product_service/client/InventoryClient.java
@@ -1,6 +1,7 @@
 package com.alejandro.product_service.client;
 
 import com.alejandro.product_service.domain.Product;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +13,18 @@ import java.util.List;
 public interface InventoryClient {
 
     @GetMapping("/inventory/sku/{sku}")
+    @CircuitBreaker(name = "getInventoryBySku", fallbackMethod = "fallbackInventoryBySku")
     ResponseEntity<List<Product>> getInventoryBySku(@PathVariable(name = "sku") String sku);
 
     @GetMapping("/inventory/uniqId/{uniqId}")
+    @CircuitBreaker(name = "getInventoryByUniqId", fallbackMethod = "fallbackInventoryByUniqId")
     ResponseEntity<List<Product>> getInventoryByUniqId(@PathVariable(name = "uniqId") String uniqId);
+
+    default List<Product> fallbackInventoryBySku(String sku, Throwable ex) {
+        return List.of(new Product());
+    }
+
+    default List<Product> fallbackInventoryByUniqId(String uniqId, Throwable ex) {
+        return List.of(new Product());
+    }
 }
